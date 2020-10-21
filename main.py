899dabd76a39948cb8fb30cc089ee53e15c4e022
Chidchanok B <chidchanok.buj@gmail.com>
2020-10-21 23:08:00 +0700

    Add venv and Create Tabs for each purpose

--- main.py
+++ main.py
@@ -4,28 +4,42 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from tkinter import *
+from tkinter import ttk
 
-
-def clicked():
-    lable = Label(window, text="Hello New Clicked", font=("Arial Bold", 20))
-    lable.grid(column=0, row=0)
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     window = Tk()
 
     window.title("Welcome to ITCS428 Project")
     window.geometry('750x450')
 
-    lbl = Label(window, text="Hello", font=("Arial Bold", 20))
+    tabControl = ttk.Notebook(window)
 
-    lbl.grid(column=0, row=0)
+    dnsTab = ttk.Frame(tabControl)
+    ftpTab = ttk.Frame(tabControl)
+    emailTab = ttk.Frame(tabControl)
+    webTab = ttk.Frame(tabControl)
 
-    btn = Button(window, text="Click Me", command=clicked)
+    tabControl.add(dnsTab, text='DNS')
+    tabControl.add(ftpTab, text='FTP')
+    tabControl.add(emailTab, text='Email')
+    tabControl.add(webTab, text='Web')
+    tabControl.pack(expand=1, fill="both")
 
-    btn.grid(column=1, row=0)
+    ttk.Label(dnsTab,
+              text="Welcome to \
+    GeeksForGeeks").grid(column=0,
+                         row=0,
+                         padx=30,
+                         pady=30)
+
+    # lable = Label(dnsTab, text="DNS", font=("Arial Bold", 10)).place(x=375, y=20)
+    # # lable.grid(column=2, row=0)
+    # search = Label(dnsTab, text="Search:").place(x=255, y=50)
+    # v = StringVar()
+    # input = Entry(dnsTab, width=25, textvariable=v)
+    # input.pack()
+    # input.grid(column=1, row=0)
+    # input = v.get()
+    # goBtn = Button(dnsTab, text="Go!", command=goDNS(input)).place(x=450, y=47)
 
     window.mainloop()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
