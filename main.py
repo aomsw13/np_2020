87765b8e1076e515831db4584ddc3a647fc8a9a4
Chidchanok B <chidchanok.buj@gmail.com>
2020-10-29 09:39:42 +0700

    Add DNS

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 
 from tkinter import *
 from tkinter import ttk
+import dns as dns
+import dns.resolver
 
 if __name__ == '__main__':
     window = Tk()
@@ -25,21 +27,29 @@ if __name__ == '__main__':
     tabControl.add(webTab, text='Web')
     tabControl.pack(expand=1, fill="both")
 
-    ttk.Label(dnsTab,
-              text="Welcome to \
-    GeeksForGeeks").grid(column=0,
-                         row=0,
-                         padx=30,
-                         pady=30)
+    # DNS
 
-    # lable = Label(dnsTab, text="DNS", font=("Arial Bold", 10)).place(x=375, y=20)
-    # # lable.grid(column=2, row=0)
-    # search = Label(dnsTab, text="Search:").place(x=255, y=50)
-    # v = StringVar()
-    # input = Entry(dnsTab, width=25, textvariable=v)
-    # input.pack()
-    # input.grid(column=1, row=0)
-    # input = v.get()
-    # goBtn = Button(dnsTab, text="Go!", command=goDNS(input)).place(x=450, y=47)
+    lable = Label(dnsTab, text="DNS : ")
+    lable.pack()
+    lable.grid(column=0, row=0)
+    inputDns = Entry(dnsTab, width=10)
+    inputDns.grid(column=1, row=0)
+
+    def searchDns():
+        lable.configure(text="DNS : ")
+        result = dns.resolver.resolve(inputDns.get(), 'MX')
+        for data in result:
+            outputDNS = ttk.Label(text=data.exchange.to_text())
+            outputDNS.pack()
+
+    btnDns = Button(dnsTab, text="Search", command=searchDns)
+    btnDns.grid(column=2, row=0)
+
+    # ttk.Label(dnsTab,
+    #           text="Welcome to \
+    # GeeksForGeeks").grid(column=0,
+    #                      row=0,
+    #                      padx=30,
+    #                      pady=30)
 
     window.mainloop()
