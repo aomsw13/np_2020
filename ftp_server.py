e346481a477e942fc6a302612c727287f347e11a
aomsw13 <disneybabyaom@hotmail.com>
2020-11-01 22:43:05 +0700

    Update ftp_server.py
    already allow client to download and upload file

--- ftp_server.py
+++ ftp_server.py
@@ -8,7 +8,7 @@ import struct
 print("\nWelcome to the FTP server.\n\nTo get started, connect a client.")
 
 # Initialise socket stuff
-TCP_IP = "192.168.1.40" # Only a local server
+TCP_IP = "10.98.4.146" # Only a local server
 TCP_PORT = 8000 # Just a random choice
 BUFFER_SIZE = 1024 # Standard size
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,6 +47,38 @@ def upld():
     conn.close()
     return
 
+def dwld():
+    conn.send("server send ok 1".encode())
+    file_name_length = struct.unpack("h", conn.recv(2))[0]
+    print('heelo server')
+    print(file_name_length)
+    file_name = conn.recv(file_name_length)
+    print(file_name.decode())
+    if os.path.isfile(file_name):
+        # Then the file exists, and send file size
+        conn.send(struct.pack("i", os.path.getsize(file_name)))
+    else:
+        # Then the file doesn't exist, and send error code
+        print("File name not valid")
+        conn.send(struct.pack("i", -1))
+        return
+    # Wait for ok to send file
+    conn.recv(BUFFER_SIZE)
+    # Enter loop to send file
+    start_time = time.time()
+    print("Sending file...")
+    content = open(file_name, "rb")
+    # Again, break into chunks defined by BUFFER_SIZE
+    l = content.read(BUFFER_SIZE)
+    while l:
+        conn.send(l)
+        l = content.read(BUFFER_SIZE)
+    content.close()
+    # Get client go-ahead, then send download details
+    conn.recv(BUFFER_SIZE)
+    conn.send(struct.pack("f", time.time() - start_time))
+    return
+
 
 while True:
     # Enter into a while loop to recieve commands from client
@@ -62,5 +94,5 @@ while True:
         upld()
     elif data == "LIST":
         list_files()
-    elif data == "DWLD":
+    elif data.decode() == "DWLD":
         dwld()
