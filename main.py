b83ae9cfa337ee7e5e3ba7fdcdfa1f8abce5a499
aomsw13 <disneybabyaom@hotmail.com>
2020-11-01 11:55:08 +0700

    Update main.py
    implement listing function to list all files when a user click a download button

--- main.py
+++ main.py
@@ -12,27 +12,40 @@ import tkinter as tk
 import socket
 import struct
 
+# class PLG(tk.Frame):
+#     def __init__(self, parent, controller):
+#         ...
+#         tk.Button(self, text="Restart", command=self.restart)
+#         tk.Button(self, text="Refresh", command=self.refresh)
+#         ...
+#
+#     def restart(self):
+#         self.refresh()
+#         self.controller.show_frame("StartPage")
+#
+#     def refresh(self):
+#         self.weight_entry.delete(0, "end")
+#         self.text.delete("1.0", "end")
+
+# Initialise socket stuff
+TCP_IP = "10.98.4.36"  # Only a local server
+TCP_PORT = 8000  # Just a random choice
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 BUFFER_SIZE = 1024  # Standard chioce
 
 # create connection fro ftp client
-def conn(file):
-    # Initialise socket stuff
-    TCP_IP = "10.98.4.36"  # Only a local server
-    TCP_PORT = 8000  # Just a random choice
-    # BUFFER_SIZE = 1024  # Standard chioce
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+def conn():
     # Connect to the server
     print("Sending server request...")
     try:
         s.connect((TCP_IP, TCP_PORT))
         print("Connection sucessful")
-        temp_status = upload_server(file,s)
+        # temp_status = upload_server(file,s)
     except:
         print("Connection unsucessful. Make sure the server is online.")
-    return temp_status
+    #return temp_status
 
-def upload_server(file_name,s):
+def upload_server(file_name):
     # Upload a file
     file_name = file_name.encode()
     print("\nUploading file: {}...".format(file_name))
@@ -107,12 +120,110 @@ def uploadFile(input_entry):
     # insert new_text at position 0
     input_entry.insert(0, new_text)
 
-    upload_status = conn(filename) # create connection to ftp server
+    conn() # create connection to ftp server
+    upload_status = upload_server(filename)
 
     print(upload_status)
 
     lable_success = Label(window, text=upload_status, font=("Arial Bold", 15))
     lable_success.grid(column=2, row=4)
+
+def listFile():
+
+    conn() # create connection to ftp server first
+
+    # List the files avaliable on the file server
+    # Called list_files(), not list() (as in the format of the others) to avoid the standard python function list()
+    print("Requesting files...\n")
+    try:
+        # Send list request
+        s.send("LIST".encode())
+    except:
+        print("Couldn't make server request. Make sure a connection has bene established.")
+        return
+    try:
+        # store list of files
+        file_list = []
+        # First get the number of files in the directory
+        number_of_files = struct.unpack("i", s.recv(4))[0]
+        print('client recv total file', number_of_files)
+        # Then enter into a loop to recieve details of each, one by one
+        for i in range(int(number_of_files)):
+            # Get the file name size first to slightly lessen amount transferred over socket
+            file_name_size = struct.unpack("i", s.recv(4))[0]
+            file_name = s.recv(file_name_size)
+            # print('client recv file ', file_name, file_name_size)
+            # Also get the file size for each item in the server
+            file_size = struct.unpack("i", s.recv(4))[0]
+            print("\t client recv file {} - {}b".format(file_name, file_size))
+            # file_list.append(file_name.decode())
+            label_file = Label(window, text=file_name, font=("Arial Bold", 10))
+            label_file.grid(column=1, row=5+i)
+            # Make sure that the client and server are syncronised
+            s.send("1".encode())
+
+        # Get total size of directory
+        total_directory_size = struct.unpack("i", s.recv(4))[0]
+        print("Total directory size: {}b".format(total_directory_size))
+
+    except:
+        print("Couldn't retrieve listing")
+        return
+    try:
+        # Final check
+        s.send("1".encode())
+        return
+    except:
+        print("Couldn't get final server confirmation")
+        return
+
+# def dwld(file_name):
+#     # Download given file
+#     print "Downloading file: {}".format(file_name)
+#     try:
+#         # Send server request
+#         s.send("DWLD")
+#     except:
+#         print "Couldn't make server request. Make sure a connection has bene established."
+#         return
+#     try:
+#         # Wait for server ok, then make sure file exists
+#         s.recv(BUFFER_SIZE)
+#         # Send file name length, then name
+#         s.send(struct.pack("h", sys.getsizeof(file_name)))
+#         s.send(file_name)
+#         # Get file size (if exists)
+#         file_size = struct.unpack("i", s.recv(4))[0]
+#         if file_size == -1:
+#             # If file size is -1, the file does not exist
+#             print "File does not exist. Make sure the name was entered correctly"
+#             return
+#     except:
+#         print "Error checking file"
+#     try:
+#         # Send ok to recieve file content
+#         s.send("1")
+#         # Enter loop to recieve file
+#         output_file = open(file_name, "wb")
+#         bytes_recieved = 0
+#         print "\nDownloading..."
+#         while bytes_recieved < file_size:
+#             # Again, file broken into chunks defined by the BUFFER_SIZE variable
+#             l = s.recv(BUFFER_SIZE)
+#             output_file.write(l)
+#             bytes_recieved += BUFFER_SIZE
+#         output_file.close()
+#         print "Successfully downloaded {}".format(file_name)
+#         # Tell the server that the client is ready to recieve the download performance details
+#         s.send("1")
+#         # Get performance details
+#         time_elapsed = struct.unpack("f", s.recv(4))[0]
+#         print "Time elapsed: {}s\nFile size: {}b".format(time_elapsed, file_size)
+#     except:
+#         print "Error downloading file"
+#         return
+#     return
+
 
 def clicked_DNS():
     lable = Label(window, text="Welcome to DNS", font=("Arial Bold", 20))
@@ -134,7 +245,7 @@ def clicked_FTP():
     upload_FTP.grid(column=2, row=2)
 
     # DOWNLOAD button
-    download_FTP = Button(window, text="DOWNLOAD", width=10, height=2)
+    download_FTP = Button(window, text="DOWNLOAD", command=lambda: listFile(), width=10, height=2)
     download_FTP.grid(column=4, row=2)
 
 
@@ -172,5 +283,5 @@ if __name__ == '__main__':
 
 
     window.mainloop()
-
+    window.update()
     # See PyCharm help at https://www.jetbrains.com/help/pycharm/
