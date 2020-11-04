6084aa1a8539395a9a592094e91bb5954732f3c7
Chidchanok B <chidchanok.buj@gmail.com>
2020-11-04 13:20:48 +0700

    Reformat and Fix Typo

--- np_project_pattern.py
+++ np_project_pattern.py
@@ -6,17 +6,14 @@
 # For GUI Part
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk, filedialog, Menu
-from tkinter.filedialog import askopenfilename
+from tkinter import ttk, filedialog
 from tkinter import messagebox
 
 # For DNS Part
-import argparse
 import dns as dns
 import dns.resolver
 import dns.reversename
 import socketserver, socket
-from socket import AF_INET, SOCK_STREAM
 
 # For FTP Part
 import os, time, struct, sys
@@ -26,13 +23,12 @@ from email.parser import BytesParser, Parser
 from email.policy import default
 
 # HTTP Part
-try:
-    from http_parser.parser import HttpParser
-except ImportError:
-    from http_parser.pyparser import HttpParser
+from html.parser import HTMLParser
+from xml.etree import ElementTree
+
 
 def searchDns():
-    lable.configure(text="DNS : ")
+    label_web.configure(text="DNS : ")
     # IPv4 Records
     ipv4 = dns.resolver.resolve(inputDns.get(), 'A')
     title_ipv4 = ttk.Label(text='IPv4 Record for %s is: ' % inputDns.get())
@@ -64,7 +60,7 @@ def searchDns():
     for data in name:
         output_name = ttk.Label(text=data.to_text())
         output_name.pack()
-        
+
     # Find FQDN 
     title_fqdn = ttk.Label(text='FQDN for %s is: ' % inputDns.get())
     title_fqdn.pack()
@@ -76,25 +72,27 @@ def searchDns():
 def now():
     return time.ctime(time.time())
 
+
 BUFFER_SIZE = 1024
 
-#FTP: Initialise socket stuff
-TCP_IP = "192.168.1.40"
-#TCP_IP = "10.62.16.47"  # Only a local server
+# FTP: Initialise socket stuff
+TCP_IP = "10.62.16.47"  # Only a local server
 TCP_PORT = 8000  # Just a random choice
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-BUFFER_SIZE = 1024  # Standard chioce
+BUFFER_SIZE = 1024  # Standard choice
+
 
 # FTP: create connection between ftp client and ftp server
 def conn():
     print("Sending server request...")
     try:
         s.connect((TCP_IP, TCP_PORT))
-        print("Connection sucessful")
+        print("Connection successful")
         # temp_status = upload_server(file,s)
     except:
-        print("Connection unsucessful. Make sure the server is online.")
+        print("Connection unsuccessful. Make sure the server is online.")
     # return temp_status
+
 
 # FTP: open dialog file from computer
 def openDialogFile(input_entry):
@@ -114,7 +112,8 @@ def openDialogFile(input_entry):
 
     return filename
 
- # FTP: start sending a file to server from client
+
+# FTP: start sending a file to server from client
 def upload_server(file_name):
     # Upload a file
     file_name = file_name.encode()
@@ -169,6 +168,7 @@ def upload_server(file_name):
         return "Cannot upload file to server"
     return "Success to upload file to server"
 
+
 # FTP: open file dialog to allow a user select file to upload
 def listUploadFile(input_entry):
     # open file dialog
@@ -180,9 +180,10 @@ def listUploadFile(input_entry):
 
     print(upload_status)
 
-    lable_success = Label(ftpTab, text=upload_status, font=("Arial Bold", 15))
-    lable_success.grid(column=1, row=7)
-    messagebox.showinfo("Upload Success!", upload_status)   
+    label_success = Label(ftpTab, text=upload_status, font=("Arial Bold", 15))
+    label_success.grid(column=1, row=7)
+    messagebox.showinfo("Upload Success!", upload_status)
+
 
 # FTP: list all files from server to client for download
 def listDownloadFile(input_entry):
@@ -193,9 +194,10 @@ def listDownloadFile(input_entry):
 
     download_status = download_server(selected_file)
 
-    lable_success = Label(ftpTab, text=download_status, font=("Arial Bold", 15))
-    lable_success.grid(column=1, row=7)
+    label_success = Label(ftpTab, text=download_status, font=("Arial Bold", 15))
+    label_success.grid(column=1, row=7)
     messagebox.showinfo("Download Success!", download_status)
+
 
 # FTP: start download file from server to client
 def download_server(file_name):
@@ -223,20 +225,20 @@ def download_server(file_name):
     except:
         print("Error checking file")
     try:
-        # Send ok to recieve file content
+        # Send ok to receive file content
         s.send("1".encode())
-        # Enter loop to recieve file
-        output_file = open('out'+file_name, "wb")
-        bytes_recieved = 0
+        # Enter loop to receive file
+        output_file = open('out' + file_name, "wb")
+        bytes_received = 0
         print("\nDownloading...")
-        while bytes_recieved < file_size:
+        while bytes_received < file_size:
             # Again, file broken into chunks defined by the BUFFER_SIZE variable
             l = s.recv(BUFFER_SIZE)
             output_file.write(l)
-            bytes_recieved += BUFFER_SIZE
+            bytes_received += BUFFER_SIZE
         output_file.close()
         print("Successfully downloaded {}".format(file_name))
-        # Tell the server that the client is ready to recieve the download performance details
+        # Tell the server that the client is ready to receive the download performance details
         s.send("1".encode())
         # Get performance details
         time_elapsed = struct.unpack("f", s.recv(4))[0]
@@ -246,24 +248,23 @@ def download_server(file_name):
         return "client can not download file"
     return "Success to download file from server"
 
-def sending (from_add, to_add, subject, message):
 
-    myHost = '192.168.1.40'   
-    #myHost = '10.62.16.47'
+def sending(from_add, to_add, subject, message):
+    myHost = '10.62.16.47'
     myPort = 8080
-    sockobj = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    sockobj.bind((myHost, myPort)) 
-    sockobj.listen(5)
+    sockObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockObj.bind((myHost, myPort))
+    sockObj.listen(5)
     headers = Parser(policy=default).parsestr(
-            'From: ' + from_add +'\n '
-            'To:'+ to_add+'\n'
-            'Subject:'+ subject +'\n'
-            '\n'
-            +(message)+'\n')
+        'From: ' + from_add + '\n '
+                              'To:' + to_add + '\n'
+                                               'Subject:' + subject + '\n'
+                                                                      '\n'
+        + message + '\n')
     print(headers)
     while True:
-        connection, address = sockobj.accept() 
-        print ('Server connected by', address)
+        connection, address = sockObj.accept()
+        print('Server connected by', address)
         while 1:
             data = connection.recv(1024)
             if not data: break
@@ -271,72 +272,35 @@ def sending (from_add, to_add, subject, message):
             connection.send(message)
     connection.close()
 
-def http_server(url_link):
-    
-    p = HttpParser()
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    body = []
-    try:
-        s.connect((url_link, 80))
-        s.send(b"GET / HTTP/1.1\r\nHost: {url_link} \r\n\r\n")
 
-        while True:
-            data = s.recv(1024)
-            if not data:
-                break
-
-            recved = len(data)
-            nparsed = p.execute(data, recved)
-            assert nparsed == recved
-
-            if p.is_headers_complete():
-                header_split = str(p.get_headers()).split("), ")
-                for i in range(len(header_split)):
-                    print(header_split[i], '\n')
-            if p.is_partial_body():
-                body.append(p.recv_body())
-
-            if p.is_message_complete():
-                break
-
-        #print ("".join(body))
-
-    finally:
-        s.close()
-
-def uri_split(uri,sender_email):
-    
+def uri_split(uri, sender_email):
     # URI Split for Email
-    uri_all = uri.get().split(":",1)
-    if(uri_all[0] == 'mailto'):
-        mail_split = uri_all[1].split("?subject=",1)
+    uri_all = uri.get().split(":", 1)
+    if uri_all[0] == 'mailto':
+        mail_split = uri_all[1].split("?subject=", 1)
         mail = mail_split[0]
         subject_split = mail_split[1].split("&body=", 1)
         subject = subject_split[0]
         body = subject_split[1]
-        sending(sender_email.get(),mail,subject,body)
-        lable_success = Label(webTab, text='Email already send, please check to your mail sent section')
-        lable_success.grid(column=1, row=_)  
-        messagebox.showinfo("Email Sending Success!", 'Email already send, please check to your mail sent section') 
+        sending(sender_email.get(), mail, subject, body)
 
-    elif(uri_all[0] == 'ftp'):
+    elif uri_all[0] == 'ftp':
         print(uri_all[1])
-        ftp_split = uri_all[1].split("//",1)
-        ip_split = ftp_split[1].split(":",1)
+        ftp_split = uri_all[1].split("//", 1)
+        ip_split = ftp_split[1].split(":", 1)
         ip = ip_split[0]
-        port_split = ip_split[1].split("/",2)
+        port_split = ip_split[1].split("/", 2)
         port = port_split[0]
         path = port_split[1]
         filename = port_split[2]
-    
-    elif(uri_all[0] == 'http' or uri_all[0] == 'https'):
+
+    elif uri_all[0] == 'http' or uri_all[0] == 'https':
         url_split = uri_all[1].split("//", 1)
         url_link = url_split[1]
-        print(url_link)
-        http_server(url_link)
+        print(url_split[1])
+
 
 if __name__ == '__main__':
-    
     window = Tk()
 
     window.title("Welcome to ITCS428 Project")
@@ -356,9 +320,9 @@ if __name__ == '__main__':
     tabControl.pack(expand=1, fill="both")
 
     # DNS Tab
-    lable = Label(dnsTab, text="DNS : ")
-    lable.pack()
-    lable.grid(column=0, row=0)
+    label_web = Label(dnsTab, text="DNS : ")
+    label_web.pack()
+    label_web.grid(column=0, row=0)
     inputDns = Entry(dnsTab, width=50)
     inputDns.grid(column=1, row=0)
 
@@ -366,9 +330,9 @@ if __name__ == '__main__':
     btnDns.grid(column=2, row=0)
 
     # FTP Tab
-    lable = Label(ftpTab, text="Select File ")
-    lable.pack()
-    lable.grid(column=0, row=0)
+    label_web = Label(ftpTab, text="Select File ")
+    label_web.pack()
+    label_web.grid(column=0, row=0)
     inputFtp = Entry(ftpTab, width=45)
     inputFtp.grid(column=1, row=0)
 
@@ -379,50 +343,45 @@ if __name__ == '__main__':
     btnFtp_download.grid(column=3, row=0)
 
     # Email Tab
-    lable = Label(emailTab, text="From ")
-    lable.pack()
-    lable.grid(column=0, row=1)
+    label_web = Label(emailTab, text="From ")
+    label_web.pack()
+    label_web.grid(column=0, row=1)
     inputFrom = Entry(emailTab, width=50)
     inputFrom.grid(column=1, row=1)
 
-    lable = Label(emailTab, text="To ")
-    lable.grid(column=0, row=2)
+    label_web = Label(emailTab, text="To ")
+    label_web.grid(column=0, row=2)
     inputTo = Entry(emailTab, width=50)
     inputTo.grid(column=1, row=2)
 
-    lable = Label(emailTab, text="Subject ")
-    lable.grid(column=0, row=3)
+    label_web = Label(emailTab, text="Subject ")
+    label_web.grid(column=0, row=3)
     inputSubject = Entry(emailTab, width=50)
     inputSubject.grid(column=1, row=3)
 
-    lable = Label(emailTab, text="Message ")
-    lable.grid(column=0, row=4)
+    label_web = Label(emailTab, text="Message ")
+    label_web.grid(column=0, row=4)
     message_body = Text(emailTab, width=65, height=10)
     message_body.grid(column=1, row=4)
 
-    btnFtp_upload = Button(emailTab, text="Send", command=lambda: sending(inputFrom.get(), inputTo.get(), inputSubject.get(), message_body.get('1.0',END)))
+    btnFtp_upload = Button(emailTab, text="Send",
+                           command=lambda: sending(inputFrom.get(), inputTo.get(), inputSubject.get(),
+                                                   message_body.get('1.0', END)))
     btnFtp_upload.grid(column=1, row=5)
 
     # Web Client Tab
-    lable = Label(webTab, text="URI : ")
-    lable.pack()
-    lable.grid(column=0, row=0)
+    label_web = Label(webTab, text="URI : ")
+    label_web.pack()
+    label_web.grid(column=0, row=0)
     inputWeb = Entry(webTab, width=50)
     inputWeb.grid(column=1, row=0)
 
-    lable = Label(webTab, text="Sender Email : ")
-    lable.grid(column=0, row=1)
+    label_web = Label(webTab, text="Sender Email : ")
+    label_web.grid(column=0, row=1)
     inputSender = Entry(webTab, width=50)
     inputSender.grid(column=1, row=1)
 
-    btnWeb = Button(webTab, text="Enter", command=lambda: uri_split(inputWeb,inputSender))
+    btnWeb = Button(webTab, text="Enter", command=lambda: uri_split(inputWeb, inputSender))
     btnWeb.grid(column=2, row=0)
-
-    # ttk.Label(dnsTab,
-    #           text="Welcome to \
-    # GeeksForGeeks").grid(column=0,
-    #                      row=0,
-    #                      padx=30,
-    #                      pady=30)
 
     window.mainloop()
