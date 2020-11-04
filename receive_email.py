d9954ba5372e158c9908bdf859beb6e3fc71ab64
minerza1 <55042768+minerza1@users.noreply.github.com>
2020-11-04 11:40:58 +0700

    new_version_receive_email.py

--- receive_email.py
+++ receive_email.py
@@ -16,23 +16,35 @@ def receive():
     # or text from cmd line args 2..n # one message for each arg listed
     # make a TCP/IP socket object
     # connect to server machine and port
+    
     for line in message: 
         sockobj.send(line.encode('utf-8'))
         data = sockobj.recv(1024)
-    Info_receive = Label(frame,text = data.decode('utf-8'))
+    Info_receive = Label(text = data.decode('utf-8'))
     Info_receive.pack()
+    text = Label(text = '------------------------ Last email --------------------------- \n')
+    text.pack()
+    f = open('data.txt','r',encoding='utf8')
+    info_email = f.read()
+    Last_email = Label(text = info_email)
+    Last_email.pack(side= LEFT)
+    
+   
+    f.close()
+   
+    
     # print ('Client received:', data.decode('utf-8'))
         
-
+    
     sockobj.close()
-
+    # a = Label(text = '---------------------------------------------------- ')
+    # a.pack()
 window = Tk()
 window.title("Receive")
 window.geometry("430x550")
-frame=Frame(window)
-frame.pack()
-text_receive = Label(frame, text = 'Receive')
-text_receive.pack()
-Button1=Button(frame,text="Receive",fg='red', command = receive)
-Button1.pack()
+
+text_receive = Label(window, text = 'Receive')
+text_receive.pack(padx=30,pady=10)
+Button1=Button(window,text="Receive",fg='red', command = receive)
+Button1.pack(padx=10)
 window.mainloop()
